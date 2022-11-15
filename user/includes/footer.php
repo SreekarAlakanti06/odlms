dc5af719cc1bb2e57abaca6342b95cbd38bb1daa
SreekarAlakanti06 <alakantisreeakr31@gmail.com>
2022-11-15 12:21:57 -0500

    first commit

---
+++ user/includes/footer.php
@@ -0,0 +1,8 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+      <div class="clearfix">
+        
+        <div class="copyright pull-left">Virtual Diagnostics System</div>
+      </div>
+    </footer>
+  </div>
